a812eb0f12aae576929c349ba600b5a04d382588
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2022-06-25 01:55:06 -0500

    update Annihilation.php

--- src/annihilation/Annihilation.php
+++ src/annihilation/Annihilation.php
@@ -1,6 +1,26 @@
 <?php
 namespace annihilation;
 
+/**
+ * Class Annihilation
+ * @package annihilation
+ */
 class Annihilation extends \pocketmine\plugin\PluginBase {
-
+  
+  /** @var Annihilation $plugin */
+  private static Annihilation $plugin;
+  
+  /**
+   * @return void
+   */
+  public function onLoad(): void {
+    self::$plugin = $this;
+  }
+  
+  /**
+   * @return Annihilation
+   */
+  public static function getPlugin(): Annihilation {
+    return self::$plugin;
+  }
 }
