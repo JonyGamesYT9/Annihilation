a3320e7648d25dc00e21de07d214f5df8c2c2df4
Jony <68771903+JonyGamesYT9@users.noreply.github.com>
2022-06-25 17:42:04 -0500

    update Annihilation.php

--- src/annihilation/Annihilation.php
+++ src/annihilation/Annihilation.php
@@ -2,6 +2,8 @@
 namespace annihilation;
 
 use annihilation\game\GameManager;
+use function is_dir;
+use function mkdir;
 
 /**
  * Class Annihilation
@@ -21,6 +23,20 @@ class Annihilation extends \pocketmine\plugin\PluginBase {
   public function onLoad(): void {
     self::$plugin = $this;
     $this->gameManager = new GameManager();
+  }
+  
+  /**
+   * @return void
+   */
+  public function onEnable(): void {
+    if (!is_dir($this->getDataFolder() . "games")) {
+      mkdir($this->getDataFolder() . "games");
+    }
+    if (!is_dir($this->getDataFolder() . "worlds")) {
+      mkdir($this->getDataFolder() . "worlds");
+    }
+    
+    
   }
   
   /**
